c3ed02a8a95031b50afcb44628f76fee7d9c5dd4
Bushra Khan <bushrakhan2212002@gmail.com>
2022-10-19 22:05:43 +0530

    homescreen

---
+++ client/src/components/Navbar/navindex.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+  NavLogo
+} from './NavbarElements';
+import logo from '../../Images/logo.png';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLogo to='/'>
+            <img src={logo} alt="logo" width="130" height="130" /> 
+         </NavLogo>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/Home' activeStyle>
+            Home
+          </NavLink>
+          <NavLink to='/Cuisine' activeStyle>
+            Cuisine
+          </NavLink>
+          <NavLink to='/Dhabas' activeStyle>
+            Dhabas
+          </NavLink>
+          <NavLink to='/About Us' activeStyle>
+            About Us
+          </NavLink>
+          <NavLink to='/Contact Us' activeStyle>
+            Contact Us
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/login'>Log In</NavBtnLink>
+        </NavBtn>
+        <NavBtn>
+          <NavBtnLink to='/register'>Register</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
